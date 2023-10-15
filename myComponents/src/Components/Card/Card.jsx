a2885c43e28fe2f22d0714d3b1fcf22d1fc80fee
Bad-Astronomer <kedarmakulkarni@gmail.com>
2023-10-15 15:13:18 +0530

    Working card loading

--- myComponents/src/Components/Card/Card.jsx
+++ myComponents/src/Components/Card/Card.jsx
@@ -1,10 +1,41 @@
 import "./card.css";
+import { useState } from "react";
 
-const Card = (props) => {
+// props = {
+//     image: imageURL,
+//     hero: headerText,
+//     subs: subtitles,
+//     buttonText: buttonText,
+// }
+
+const defaultProps = {
+    image: "/src/Components/Card/testImage.jpg",
+    hero: "TEST HERO",
+    subs: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sapiente.",
+    buttonText: "Button",
+}
+
+const Card = (props = {}) => {
+    props.hero ? "" : props = defaultProps;
+
+    const maxWidth = 240;
+    const [dimension, setDimension] = useState([0, 0])
+
+    const handleImageLoad = (event) => {
+        const { naturalWidth, naturalHeight } = event.target;
+        let aspectRatio = naturalHeight / naturalWidth;
+        setDimension([ maxWidth * aspectRatio, maxWidth ]);
+    }
+
     return (
-        <div className="card-component">
+        <div className="card-component" style = {{width: dimension[1]}}>
             <div className="card-image">
-                <img src={props.image} alt="" />
+                <img src={props.image}
+                    alt="Image"
+                    onLoad={handleImageLoad}
+                    height = {dimension[0]}
+                    width = {dimension[1]}
+                />
             </div>
             <div className="card-hero">
                 {props.hero}
